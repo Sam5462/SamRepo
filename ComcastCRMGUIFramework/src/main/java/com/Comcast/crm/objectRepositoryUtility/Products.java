c00340fbd231d7fcf50bcb76f4fe0a91338c3229
Mritunjay616 <mritunjaykumar616@gmail.com>
2024-09-23 16:00:03 +0530

    prod class modified by sam

--- ComcastCRMGUIFramework/src/main/java/com/Comcast/crm/objectRepositoryUtility/Products.java
+++ ComcastCRMGUIFramework/src/main/java/com/Comcast/crm/objectRepositoryUtility/Products.java
@@ -1,4 +1,15 @@
 package com.Comcast.crm.objectRepositoryUtility;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class Products {
 
+	public WebElement getCreateProductImgBtn() {
+		return createProductImgBtn;
+	}
+
+	@FindBy(xpath="//img[@alt='Create Product...']")
+	private WebElement createProductImgBtn;
 }
+
